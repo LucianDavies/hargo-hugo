0dc3741fb57c37c864621cac09359d65fe40c27d
LucianDavies <5219738+LucianDavies@users.noreply.github.com>
2022-05-31 00:11:02 +0200

    Updated: 2022-05-31 00:11:02

--- configure.sh
+++ configure.sh
@@ -28,7 +28,7 @@ cat << EOF > "${SCRIPT_PATH}/config.toml"
 ######################## default configuration ####################
 baseURL = "https://tinywebshop.co/${Store_Name_Lowercase}/"
 languageCode = "en"
-title = ${title}
+title = "TWS | ${Store_Name}"
 theme = "hargo-hugo"
 summaryLength = "20"
 paginate = 10
