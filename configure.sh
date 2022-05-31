441c08d3cf260adfcfcf978d7bfeeee5b73e9932
LucianDavies <5219738+LucianDavies@users.noreply.github.com>
2022-05-31 22:21:33 +0200

    Updated: 2022-05-31 22:21:32

--- configure.sh
+++ configure.sh
@@ -26,8 +26,8 @@ echo "$Store_Name_Lowercase"
 
 cat << EOF > "${SCRIPT_PATH}/config.toml"
 ######################## default configuration ####################
-baseURL = "https://lucindavies.github.io/"
-# baseURL = "https://tinywebshop.co/${Store_Name_Lowercase}"
+# baseURL = "https://lucindavies.github.io/"
+baseURL = "https://tinywebshop.co/${Store_Name_Lowercase}"
 languageCode = "en"
 title = "TWS | ${Store_Name}"
 theme = "hargo-hugo"
