69b60768cc2702c490e832d52d79b30a3a6b9d8a
LucianDavies <5219738+LucianDavies@users.noreply.github.com>
2022-06-01 08:49:51 +0200

    Updated: 2022-06-01 08:49:51

--- configure.sh
+++ configure.sh
@@ -43,6 +43,8 @@ disqusShortname = ""
   link = "plugins/slick/slick.css"
   [[params.plugins.css]]
   link = "plugins/themify-icons/themify-icons.css"
+  [[params.plugins.css]]
+  link = "scss/style.scss"
 
   # JS Plugins
   [[params.plugins.js]]
@@ -53,6 +55,8 @@ disqusShortname = ""
   link = "plugins/slick/slick.min.js"
   [[params.plugins.js]]
   link = "plugins/google-map/gmap.js"
+  [[params.plugins.js]]
+  link = "js/script.js"
 
 
 ############################## navigation ###############################
